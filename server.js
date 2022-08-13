16b7dba42d6501cab8fe8e45b1eb665224588b52
Kevin Stoop <433243@student.fontys.nl>
2022-08-13 12:10:49 +0200

    🚀 First commit
    Signed-off-by: Kevin Stoop <433243@student.fontys.nl>

---
+++ server.js
@@ -0,0 +1,11 @@
+const path = require('path');
+const express = require('express');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+})
